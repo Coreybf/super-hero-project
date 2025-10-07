c88fec914af8bd65968f8115141163e21cb51deb
Corey Furman <cfurma026@west-mec.org>
2025-10-07 14:21:05 -0700

    update day one

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # super-hero-project
 Year 2 Quater 1 Team Project - Captain
+<!-- full name regex: /^[A-Za-z]{1,20}[ ][A-Za-z]{1,20}$/g -->
+<!-- superhero name regex: /^[A-Za-z]{1,20}[ -]?[A-Za-z]{1,20}$/g -->
+<!-- super power regex:  /^[A-Za-z]{1,30}[ -]?[A-Za-z]{1,30}$/g-->
+<!-- powerlevel regex: /[\d]{1,4}/g -->
+<!-- secret idenitity: /^(true)|(false)$/g -->
+
+We got the code set up but we got to test the server
---
+++ server-db.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const heroDBpath = path.join(__dirname, 'data', 'superHeros.json');
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+const PORT = 3000;
+
+async function readHeros() {
+    try {
+        const data = await fs.readFile(heroDBpath, 'utf-8');
+        return JSON.parse(data);
+
+    }
+    catch (error) {
+        return [];
+    }
+}
+async function writeHeroes(heroes) {
+    try {
+        await fs.writeFile(heroDBpath, JSON.stringify(heroes, null, 2));
+    }
+    catch (error) {
+        console.error('problem writing to file', error);
+    }
+}
+async function initializeDatabase() {
+    try {
+        await fs.access(heroDBpath);
+
+    } catch (error) {
+        await writeHeroes([]);
+    }
+}
+
+initializeDatabase();
+
+app.post('/heros', (req, res) => {
+});
+app.listen(PORT, () => {
+    console.log(`App is listening on ${PORT}`)
+});
